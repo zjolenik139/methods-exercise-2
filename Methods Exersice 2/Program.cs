d9ae25e931bd481a8fbab8efb4eb475821dd5cb0
Zachariah Olenik <zjolenik139@gmail.com>
2022-08-08 14:19:48 -0400

    exercise 2

---
+++ Methods Exersice 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Methods_Exersice_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var result = Add(3, 5, 87);
+            var multiplicationAnswer = Multiply(10,5,10);
+            Console.WriteLine(result);
+            Console.WriteLine(multiplicationAnswer);
+        }
+        public static int Add(int num1, int num2, int num3)
+        { return num1 + num2 + num3; }
+        public static int Multiply(int num1, int num2, int num3)
+        { return num1 * num2 * num3; }
+
+
+    }
+}
